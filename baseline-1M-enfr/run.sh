d17db33a1016fcb52c66bdc34a06dac0b473f831
vince62s <vince62s@yahoo.com>
2017-01-12 13:51:12 +0100

    minor fixes

---
+++ baseline-1M-enfr/run.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+#
+# Copyright 2017 Ubiqus   (Author: Vincent Nguyen)
+#		 Systran  (Author: Jean Senellart)
+# License MIT
+#
+# This recipe shows how to build an openNMT translation model from English to French
+# based on a limited resource (1 Mio segments)
+#
+# Based on the tuto from the OpenNMT forum
+
+
+# TODO test is GPU is present or not
+CUDA_VISIBLE_DEVICES=0
+decode_cpu=false
+
+# Make symlinks to access OpenNMT scripts - change this line if needed
+OPENNMT_PATH=../../OpenNMT
+[ ! -h tools ] && ln -s $OPENNMT_PATH/tools tools
+[ ! -h preprocess.lua ] && ln -s $OPENNMT_PATH/preprocess.lua preprocess.lua
+[ ! -h train.lua ] && ln -s $OPENNMT_PATH/train.lua train.lua
+[ ! -h translate.lua ] && ln -s $OPENNMT_PATH/translate.lua translate.lua
+[ ! -h onmt ] && ln -s $OPENNMT_PATH/onmt onmt
+
+# this is usefull to skip some stages during step by step execution
+stage=0
+
+# if you want to run without training and use an existing model in the "exp" folder set notrain to true
+notrain=true
+
+# making these variables to make replication easier for other languages
+sl=en
+tl=fr
+
+# At the moment only "stage" option is available anyway
+. local/parse_options.sh
+
+# Data download and preparation
+
+if [ $stage -le 0 ]; then
+# TODO put this part in a local/download_data.sh script ?
+  mkdir -p data
+  cd data
+  if [ ! -f baseline-1M-$sl$tl.tgz ]; then
+    echo "$0: downloading the baseline corpus from amazon s3"
+    wget https://s3.amazonaws.com/opennmt-trainingdata/baseline-1M-$sl$tl.tgz
+    tar xzfv baseline-1M-$sl$tl.tgz
+  fi
+  if [ ! -f onmt_$sl$tl"_tests".tgz ]; then
+    echo "$0: downloading the baseline corpus from amazon s3"
+    wget https://s3.amazonaws.com/opennmt-tests/testsets-$sl$tl.tgz
+    tar xzfv testsets-$sl$tl.tgz
+  fi
+  cd ../local
+  if [ ! -f mteval-v13a.pl ]; then
+    wget https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts/generic/mteval-v13a.pl
+  fi
+  if [ ! -f input-from-sgm.perl ]; then
+    wget https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts/ems/support/input-from-sgm.perl
+  fi
+  if [ ! -f wrap-xml.perl ]; then
+    wget https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts/ems/support/wrap-xml.perl
+  fi
+  if [ ! -f multi-bleu.perl ]; then
+    wget https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts/generic/multi-bleu.perl
+  fi
+  cd ..
+fi
+
+# Tokenize the Corpus
+if [ $stage -le 1 ]; then
+  echo "$0: tokenizing corpus and test sets"
+  for f in data/baseline-1M-$sl$tl/*.?? ; do th tools/tokenize.lua -case_feature -joiner_annotate < $f > $f.tok ; done
+fi
+
+# Preprocess the data - decide here the vocabulary size 50000 default value
+if [ $stage -le 2 ]; then
+  mkdir -p exp
+  echo "$0: preprocessing corpus"
+  th preprocess.lua -src_vocab_size 50000 -tgt_vocab_size 50000 \
+  -train_src data/baseline-1M-$sl$tl/generic-1M_train.$sl.tok \
+  -train_tgt data/baseline-1M-$sl$tl/generic-1M_train.$tl.tok \
+  -valid_src data/baseline-1M-$sl$tl/generic_valid.$sl.tok \
+  -valid_tgt data/baseline-1M-$sl$tl/generic_valid.$tl.tok -save_data exp/model-$sl$tl
+fi
+
+# Train the model !!!! even if OS cuda device ID is 0 you need -gpuid=1
+# Decide here the number of epochs, learning rate, which epoch to start decay, decay rate
+# if you change number of epochs do not forget to change the model name too
+if [ $stage -le 3 ]; then
+  if [ $notrain = false ]; then
+    echo "$0: training starting, will take a while."
+    th train.lua -data  exp/model-$sl$tl-train.t7 \
+    -save_model exp/model-$sl$tl \
+    -epochs 13 -learning_rate 1 -start_decay_at 5 -learning_rate_decay 0.65 -gpuid 1
+    cp -f exp/model-$sl$tl"_epoch13_*.t7" exp/model-$sl$tl"_final.t7"
+  else
+    echo "$0: using an existing model"
+    if [ ! -f exp/model-$sl$tl"_final.t7" ]; then
+      echo "$0: mode file does not exist"
+      exit 1
+    fi
+  fi
+fi
+
+# Deploy model for CPU usage
+if [ $stage -le 4 ]; then
+  if [ $decode_cpu = true ]; then
+    th tools/release_model.lua -force -model exp/model-$sl$tl"_final.t7" -output_model exp/model-$sl$tl"_cpu.t7" -gpuid 1
+  fi
+fi
+
+# Translate using gpu
+# you can change this by changing the model name from _final to _cpu and remove -gpuid 1
+if [ $stage -le 5 ]; then
+  [ $decode_cpu = true ] && dec_opt="" || dec_opt="-gpuid 1"
+  th translate.lua -model exp/model-$sl$tl"_final.t7" \
+  -src data/baseline-1M-$sl$tl/generic_test.$sl.tok -output exp/generic_test.hyp.$tl.tok $dec_opt
+fi
+
+# Evaluate the generic test set with multi-bleu
+if [ $stage -le 6 ]; then
+  th tools/detokenize.lua < exp/generic_test.hyp.$tl.tok > exp/generic_test.hyp.$tl.detok
+  tools/multi-bleu.perl data/baseline-1M-$sl$tl/generic_test.$tl \
+  < exp/generic_test.hyp.$tl.detok > exp/generic_test_multibleu.txt
+fi
+
+###############################
+#### Newstest Evaluation
+####
+
+if [ $stage -le 7 ]; then
+
+testset=newstest2014-fren
+
+  tools/input-from-sgm.perl < data/testsets-$sl$tl/News/$testset-src.$sl.sgm \
+  > data/testsets-$sl$tl/News/$testset-src.$sl
+
+  th tools/tokenize.lua -case_feature -joiner_annotate < data/testsets-$sl$tl/News/$testset-src.$sl \
+  > data/testsets-$sl$tl/News/$testset-src.$sl.tok
+
+  [ $decode_cpu = true ] && dec_opt="" || dec_opt="-gpuid 1"
+
+  th translate.lua -model exp/model-$sl$tl"_final"*.t7 \
+  -src data/testsets-$sl$tl/News/$testset-src.$sl.tok \
+  -output exp/$testset-tgt.trans.$tl.tok $dec_opt
+
+
+  th tools/detokenize.lua -case_feature < exp/$testset-tgt.trans.$tl.tok \
+  > exp/$testset-tgt.trans.$tl
+
+# Wrap-xml to convert to sgm
+  tools/wrap-xml.perl $tl data/testsets-$sl$tl/News/$testset-src.$sl.sgm tst \
+  < exp/$testset-tgt.trans.$tl \
+  > exp/$testset-tgt.trans.$tl.sgm
+
+  tools/mteval-v13a.pl -r data/testsets-$sl$tl/News/$testset-ref.$tl.sgm \
+  -s data/testsets-$sl$tl/News/$testset-src.$sl.sgm -t exp/$testset-tgt.trans.$tl.sgm \
+  -c > exp/nist-bleu-$testset
+fi
+
